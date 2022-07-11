1f06839e101f6d567657127e69396565864ac5e7
anzervakis <80947480+anzervakis@users.noreply.github.com>
2022-07-11 11:44:24 +0300

    Add files via upload

---
+++ ytdownloader.py
@@ -0,0 +1,66 @@
+from pytube import (YouTube,Playlist)
+import datetime
+
+print('--------------------YOUTUBE DOWNLOADER--------------------')
+
+playlist_or_video=input('Playlist or Video: ')
+
+if playlist_or_video=='video':
+    
+    input_field=input('Enter link: ')
+
+    yt = YouTube(input_field)
+
+    views = yt.views
+
+    title = yt.title
+
+    length = yt.length
+
+    video_length = datetime.timedelta(seconds = length)
+
+    print('Video Title:',title)
+    print('Video Views:',views)
+    print('Video Length:',video_length)
+    
+    download_file_video = yt.streams.filter(file_extension="mp4").get_highest_resolution()
+
+    print('Downloading',title)
+
+    download_file_video.download()
+
+
+
+
+elif playlist_or_video=='playlist':
+    
+    input_field=input('Enter link: ')
+
+    pl = Playlist(input_field)
+
+    title_playlist=pl.title
+
+    views_playlist=pl.views
+
+    videos = len(pl.videos)
+
+    video_list=pl.video_urls
+
+    print('Playlist Length:',videos,'videos')
+    
+    videos_list_position=0
+
+    
+    for i in range(videos):
+            
+        video_for_download_get=video_list[videos_list_position]
+            
+        video_for_download=YouTube(video_for_download_get)
+            
+        video_download=video_for_download.streams.filter(file_extension="mp4").get_highest_resolution()
+            
+        print(f'Downloading {video_download.title}')
+
+        video_download.download()
+            
+        videos_list_position=videos_list_position+1
